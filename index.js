07c3c3b5d6fba3606d10eb2e3609c3e374a3a1ad
Trey <treyvis@gmail.com>
2017-07-25 22:53:05 -0600

    sending request to pixabay

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 "use strict"
 
 const express = require('express')
+const request = require('request')
 const app = express()
 const port = process.env.PORT || 4040
+
+request('https://pixabay.com/api/?key=5984160-851323f1904b8ff9fb551e650&q=yellow+flowers&image_type=photo',
+	(err, res, body) => {
+		console.log(JSON.parse(body).totalHits)
+})
+
 
 app.get('/', (req, res) => {
 	res.send('You found it')
@@ -13,5 +20,5 @@ app.get('/search/:query', (req, res) => {
 })
 
 app.listen(port, () =>{
-	console.log('Listening on ', port)
+	console.log('Listening on', port)
 })
