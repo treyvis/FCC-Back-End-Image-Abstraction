8323149772e97a6b30aacc408aa542334e24b8e7
Trey <treyvis@gmail.com>
2017-07-26 00:03:53 -0600

    adding searches to database

--- index.js
+++ index.js
@@ -8,19 +8,15 @@ const config = require('./config.json')
 const port = process.env.PORT || 4040
 const mongoURI = 'mongodb://' + config.user + ':' + config.password + '@ds111103.mlab.com:11103/image-abstraction-search'
 
-mongo.connect(mongoURI, (err, db) =>{
-	console.log('Connected to database')
-	//db.collection('searches').insert({"test":"This is my set doc for serach"})
-})
-
 app.get('/', (req, res) => {
 	res.send('You found it')
 })
 
 app.get('/search/:query', (req, res) => {
+	const query = req.params.query
 	let offset  = req.query.offset
 	if (!offset || offset > 300 || offset < 3) offset = 20
-	request('https://pixabay.com/api/?key=' + config.key + '&q=' + req.params.query + '&per_page=' + offset, (err, pixaResponse, body) => {
+	request('https://pixabay.com/api/?key=' + config.key + '&q=' + query + '&per_page=' + offset, (err, pixaResponse, body) => {
 		const searchResults = JSON.parse(body).hits
 		let resultsArray = [];
 		for (let hit in searchResults) {
@@ -32,6 +28,17 @@ app.get('/search/:query', (req, res) => {
 			resultsArray.push(resultObject)
 		}
 		res.json(resultsArray)
+		mongo.connect(mongoURI,(err, db) => {
+			if (err) throw err
+			const resultsObject = {
+				"search": query,
+				"results": resultsArray
+			}
+			db.collection('searches').insert(resultsObject, (err, doc) => {
+				if (err) throw err
+				console.log('Inserted search to database')
+			})
+		})
 	})
 })
 
