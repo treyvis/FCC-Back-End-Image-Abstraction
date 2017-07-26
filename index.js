01e32a3d6d38187c60d2826c0b6c2a382bf5de93
Trey <treyvis@gmail.com>
2017-07-25 23:54:41 -0600

    connecting to database

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
 
 const express = require('express')
 const request = require('request')
+const mongo = require('mongodb').MongoClient
 const app = express()
-const key = require('./config.json').key
+const config = require('./config.json')
 const port = process.env.PORT || 4040
+const mongoURI = 'mongodb://' + config.user + ':' + config.password + '@ds111103.mlab.com:11103/image-abstraction-search'
+
+mongo.connect(mongoURI, (err, db) =>{
+	console.log('Connected to database')
+	//db.collection('searches').insert({"test":"This is my set doc for serach"})
+})
 
 app.get('/', (req, res) => {
 	res.send('You found it')
@@ -13,7 +20,7 @@ app.get('/', (req, res) => {
 app.get('/search/:query', (req, res) => {
 	let offset  = req.query.offset
 	if (!offset || offset > 300 || offset < 3) offset = 20
-	request('https://pixabay.com/api/?key=' + key + '&q=' + req.params.query + '&per_page=' + offset, (err, pixaResponse, body) => {
+	request('https://pixabay.com/api/?key=' + config.key + '&q=' + req.params.query + '&per_page=' + offset, (err, pixaResponse, body) => {
 		const searchResults = JSON.parse(body).hits
 		let resultsArray = [];
 		for (let hit in searchResults) {
